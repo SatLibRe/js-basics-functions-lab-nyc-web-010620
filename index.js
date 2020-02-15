4f1300b6d17ba26b09df13ca3b25fbfb7bb788b6
Miles Higbie <miles.higbie@gmail.com>
2020-02-15 17:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function calculatesFarePrice(start, destination){
     case dtf < 400: 
       return 0;
     case dtf > 400 && dtf < 2000:
-      
+      return 2 * (dtf - 400); 
+    case dtf > 2000 && dtf < 2500:
+      return 
   }
 }
